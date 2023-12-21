127d3173469232cbac33f4df1a94a13013da2dab
tienv1per <tienv1perds@gmail.com>
2023-12-22 01:37:52 +0700

    add login register page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import AuthForm from "./components/AuthForm";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
 				</h2>
 			</div>
 			{/* AuthForm */}
+			<AuthForm/>
 		</div>
   );
 };
